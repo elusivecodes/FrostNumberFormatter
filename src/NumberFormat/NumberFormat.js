80614d298ccd63cb59bc53cb9c5feb3ab3464d51
elusivecodes <18050480+elusivecodes@users.noreply.github.com>
2019-04-13 15:28:49 +1000

    Updated version number

--- src/NumberFormat/NumberFormat.js
+++ src/NumberFormat/NumberFormat.js
@@ -31,7 +31,7 @@ class NumberFormat {
                 baseFormatter.format(i)
             );
 
-        const digitRegex = `[${this._digits.map(NumberFormat.regExEscape).join('|')}]`,
+        const digitRegex = `[${this._digits.map(NumberFormat._regExEscape).join('|')}]`,
             parts = this._formatter.formatToParts(-10000000.1);
 
         this._minus = parts.find(part => part.type === 'minusSign').value || '-';
@@ -40,12 +40,12 @@ class NumberFormat {
 
         let numberRegex = '';
         if (this._group) {
-            numberRegex += `(?:${digitRegex}{1,3}${NumberFormat.regExEscape(this._group)})*${digitRegex}{1,3}`;
+            numberRegex += `(?:${digitRegex}{1,3}${NumberFormat._regExEscape(this._group)})*${digitRegex}{1,3}`;
         } else {
             numberRegex += `${digitRegex}+`;
         }
 
-        numberRegex += `(?:${NumberFormat.regExEscape(this._decimal)}${digitRegex}+)?`;
+        numberRegex += `(?:${NumberFormat._regExEscape(this._decimal)}${digitRegex}+)?`;
 
         let regex = '',
             numberAdded = false;
@@ -55,9 +55,9 @@ class NumberFormat {
 
         parts.forEach(part => {
             if (['literal', 'currency'].includes(part.type)) {
-                regex += `${NumberFormat.regExEscape(part.value)}?`;
+                regex += `${NumberFormat._regExEscape(part.value)}?`;
             } else if (part.type === 'minusSign') {
-                regex += `(${NumberFormat.regExEscape(part.value)}?)`;
+                regex += `(${NumberFormat._regExEscape(part.value)}?)`;
 
                 if (numberAdded) {
                     this._minusIndex = 2;
@@ -132,7 +132,7 @@ class NumberFormat {
      * @param {string} string The string to escape.
      * @returns {string} The escaped string.
      */
-    static regExEscape(string) {
+    static _regExEscape(string) {
         return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$$&');
     }
 
