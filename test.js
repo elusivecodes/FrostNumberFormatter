9631971091050075b74bd08d2fa20c750f292575
elusivecodes <18050480+elusivecodes@users.noreply.github.com>
2019-12-21 18:56:18 +1000

    Added more tests

--- test.js
+++ test.js
@@ -4,14 +4,21 @@ const NumberFormat = require('./dist/frost-numberformat.min');
 describe('NumberFormat tests', function() {
 
     describe('#format', function() {
-        it('formats a number', function() {
+        it('formats a number to a string', function() {
             assert.equal(
-                new NumberFormat('en-US').format(100000.123),
-                '100,000.123'
+                new NumberFormat('en-US').format(123456.789),
+                '123,456.789'
             );
         });
 
         it('works with locales', function() {
+            assert.equal(
+                new NumberFormat('de-DE').format(123456.789),
+                '123.456,789'
+            );
+        });
+
+        it('works with currencies', function() {
             assert.equal(
                 new NumberFormat(
                     'en-GB',
@@ -19,21 +26,179 @@ describe('NumberFormat tests', function() {
                         style: 'currency',
                         currency: 'GBP'
                     }
-                ).format(999999.50),
-                '£999,999.50'
+                ).format(123456.78),
+                '£123,456.78'
+            );
+        });
+
+        it('works with currencies and locales', function() {
+            assert.equal(
+                new NumberFormat(
+                    'de-DE',
+                    {
+                        style: 'currency',
+                        currency: 'AUD'
+                    }
+                ).format(123456.78),
+                '123.456,78\xa0AU$'
+            );
+        });
+    });
+
+    describe('#formatToParts', function() {
+        it('returns an array of formatted parts', function() {
+            assert.deepEqual(
+                new NumberFormat('en-US').formatToParts(123456.789),
+                [
+                    {
+                        type: 'integer',
+                        value: '123'
+                    },
+                    {
+                        type: 'group',
+                        value: ','
+                    },
+                    {
+                        type: 'integer',
+                        value: '456'
+                    },
+                    {
+                        type: 'decimal',
+                        value: '.'
+                    },
+                    {
+                        type: 'fraction',
+                        value: '789'
+                    }
+                ]
+            );
+        });
+
+        it('works with locales', function() {
+            assert.deepEqual(
+                new NumberFormat('de-DE').formatToParts(123456.789),
+                [
+                    {
+                        type: 'integer',
+                        value: '123'
+                    },
+                    {
+                        type: 'group',
+                        value: '.'
+                    },
+                    {
+                        type: 'integer',
+                        value: '456'
+                    },
+                    {
+                        type: 'decimal',
+                        value: ','
+                    },
+                    {
+                        type: 'fraction',
+                        value: '789'
+                    }
+                ]
+            );
+        });
+
+        it('works with currencies', function() {
+            assert.deepEqual(
+                new NumberFormat(
+                    'en-GB',
+                    {
+                        style: 'currency',
+                        currency: 'GBP'
+                    }
+                ).formatToParts(123456.78),
+                [
+                    {
+                        type: 'currency',
+                        value: '£'
+                    },
+                    {
+                        type: 'integer',
+                        value: '123'
+                    },
+                    {
+                        type: 'group',
+                        value: ','
+                    },
+                    {
+                        type: 'integer',
+                        value: '456'
+                    },
+                    {
+                        type: 'decimal',
+                        value: '.'
+                    },
+                    {
+                        type: 'fraction',
+                        value: '78'
+                    }
+                ]
+            );
+        });
+
+        it('works with currencies and locales', function() {
+            assert.deepEqual(
+                new NumberFormat(
+                    'de-DE',
+                    {
+                        style: 'currency',
+                        currency: 'AUD'
+                    }
+                ).formatToParts(123456.78),
+                [
+                    {
+                        type: 'integer',
+                        value: '123'
+                    },
+                    {
+                        type: 'group',
+                        value: '.'
+                    },
+                    {
+                        type: 'integer',
+                        value: '456'
+                    },
+                    {
+                        type: 'decimal',
+                        value: ','
+                    },
+                    {
+                        type: 'fraction',
+                        value: '78'
+                    },
+                    {
+                        type: 'literal',
+                        value: '\xa0'
+                    },
+                    {
+                        type: 'currency',
+                        value: 'AU$'
+                    }
+                ]
             );
         });
     });
 
     describe('#parse', function() {
-        it('parses a number', function() {
+        it('parses a number from a string', function() {
             assert.equal(
-                new NumberFormat('en-US').parse('100,000.123'),
-                100000.123
+                new NumberFormat('en-US').parse('123,456.789'),
+                123456.789
             );
         });
 
         it('works with locales', function() {
+            assert.equal(
+                new NumberFormat('de-DE').parse('123.456,789'),
+                123456.789
+            );
+        });
+
+        it('works with currencies', function() {
             assert.equal(
                 new NumberFormat(
                     'en-GB',
@@ -41,8 +206,21 @@ describe('NumberFormat tests', function() {
                         style: 'currency',
                         currency: 'GBP'
                     }
-                ).parse('£999,999.50'),
-                999999.50
+                ).parse('£123,456.78'),
+                123456.78
+            );
+        });
+
+        it('works with currencies and locales', function() {
+            assert.equal(
+                new NumberFormat(
+                    'de-DE',
+                    {
+                        style: 'currency',
+                        currency: 'AUD'
+                    }
+                ).parse('123.456,78 AU$'),
+                123456.78
             );
         });
     });
