c5cd7af8e8869bd1c9ec64ce797de67706b577b6
elusivecodes <18050480+elusivecodes@users.noreply.github.com>
2020-03-07 13:34:51 +1000

    Updated NPM

--- dist/frost-numberformat-es5.js
+++ dist/frost-numberformat-es5.js
@@ -6,7 +6,7 @@ function _defineProperties(target, props) { for (var i = 0; i < props.length; i+
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
 /**
  * FrostNumberFormat v1.0.0
@@ -27,9 +27,7 @@ function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterat
    * @class
    */
 
-  var NumberFormat =
-  /*#__PURE__*/
-  function () {
+  var NumberFormat = /*#__PURE__*/function () {
     /**
      * New NumberFormat constructor.
      * @param {string|string[]} [locale] The locale(s) to use for formatting.
