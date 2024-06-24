52d7955e6fd283ef0a8a64f7c0946a50bd7cf4ba
Elusive <18050480+elusivecodes@users.noreply.github.com>
2024-06-24 11:11:37 +1000

    - Updated NPM.

--- test.js
+++ test.js
@@ -2,26 +2,25 @@ import assert from 'node:assert/strict';
 import NumberFormatter from './src/index.js';
 
 describe('NumberFormatter', function() {
-
     describe('#format', function() {
         it('formats a number to a string', function() {
             assert.strictEqual(
                 new NumberFormatter('en-us').format(123456.789),
-                '123,456.789'
+                '123,456.789',
             );
         });
 
         it('works with locales', function() {
             assert.strictEqual(
                 new NumberFormatter('de-de').format(123456.789),
-                '123.456,789'
+                '123.456,789',
             );
         });
 
         it('works with locale numerals', function() {
             assert.strictEqual(
                 new NumberFormatter('ar-eg').format(123456.789),
-                '١٢٣٬٤٥٦٫٧٨٩'
+                '١٢٣٬٤٥٦٫٧٨٩',
             );
         });
 
@@ -31,10 +30,10 @@ describe('NumberFormatter', function() {
                     'en-GB',
                     {
                         style: 'currency',
-                        currency: 'GBP'
-                    }
+                        currency: 'GBP',
+                    },
                 ).format(123456.78),
-                '£123,456.78'
+                '£123,456.78',
             );
         });
 
@@ -44,10 +43,10 @@ describe('NumberFormatter', function() {
                     'de-de',
                     {
                         style: 'currency',
-                        currency: 'AUD'
-                    }
+                        currency: 'AUD',
+                    },
                 ).format(123456.78),
-                '123.456,78\xa0AU$'
+                '123.456,78\xa0AU$',
             );
         });
     });
@@ -59,25 +58,25 @@ describe('NumberFormatter', function() {
                 [
                     {
                         type: 'integer',
-                        value: '123'
+                        value: '123',
                     },
                     {
                         type: 'group',
-                        value: ','
+                        value: ',',
                     },
                     {
                         type: 'integer',
-                        value: '456'
+                        value: '456',
                     },
                     {
                         type: 'decimal',
-                        value: '.'
+                        value: '.',
                     },
                     {
                         type: 'fraction',
-                        value: '789'
-                    }
-                ]
+                        value: '789',
+                    },
+                ],
             );
         });
 
@@ -87,25 +86,25 @@ describe('NumberFormatter', function() {
                 [
                     {
                         type: 'integer',
-                        value: '123'
+                        value: '123',
                     },
                     {
                         type: 'group',
-                        value: '.'
+                        value: '.',
                     },
                     {
                         type: 'integer',
-                        value: '456'
+                        value: '456',
                     },
                     {
                         type: 'decimal',
-                        value: ','
+                        value: ',',
                     },
                     {
                         type: 'fraction',
-                        value: '789'
-                    }
-                ]
+                        value: '789',
+                    },
+                ],
             );
         });
 
@@ -115,25 +114,25 @@ describe('NumberFormatter', function() {
                 [
                     {
                         type: 'integer',
-                        value: '١٢٣'
+                        value: '١٢٣',
                     },
                     {
                         type: 'group',
-                        value: '٬'
+                        value: '٬',
                     },
                     {
                         type: 'integer',
-                        value: '٤٥٦'
+                        value: '٤٥٦',
                     },
                     {
                         type: 'decimal',
-                        value: '٫'
+                        value: '٫',
                     },
                     {
                         type: 'fraction',
-                        value: '٧٨٩'
-                    }
-                ]
+                        value: '٧٨٩',
+                    },
+                ],
             );
         });
 
@@ -143,35 +142,35 @@ describe('NumberFormatter', function() {
                     'en-GB',
                     {
                         style: 'currency',
-                        currency: 'GBP'
-                    }
+                        currency: 'GBP',
+                    },
                 ).formatToParts(123456.78),
                 [
                     {
                         type: 'currency',
-                        value: '£'
+                        value: '£',
                     },
                     {
                         type: 'integer',
-                        value: '123'
+                        value: '123',
                     },
                     {
                         type: 'group',
-                        value: ','
+                        value: ',',
                     },
                     {
                         type: 'integer',
-                        value: '456'
+                        value: '456',
                     },
                     {
                         type: 'decimal',
-                        value: '.'
+                        value: '.',
                     },
                     {
                         type: 'fraction',
-                        value: '78'
-                    }
-                ]
+                        value: '78',
+                    },
+                ],
             );
         });
 
@@ -181,39 +180,39 @@ describe('NumberFormatter', function() {
                     'de-de',
                     {
                         style: 'currency',
-                        currency: 'AUD'
-                    }
+                        currency: 'AUD',
+                    },
                 ).formatToParts(123456.78),
                 [
                     {
                         type: 'integer',
-                        value: '123'
+                        value: '123',
                     },
                     {
                         type: 'group',
-                        value: '.'
+                        value: '.',
                     },
                     {
                         type: 'integer',
-                        value: '456'
+                        value: '456',
                     },
                     {
                         type: 'decimal',
-                        value: ','
+                        value: ',',
                     },
                     {
                         type: 'fraction',
-                        value: '78'
+                        value: '78',
                     },
                     {
                         type: 'literal',
-                        value: '\xa0'
+                        value: '\xa0',
                     },
                     {
                         type: 'currency',
-                        value: 'AU$'
-                    }
-                ]
+                        value: 'AU$',
+                    },
+                ],
             );
         });
     });
@@ -222,21 +221,21 @@ describe('NumberFormatter', function() {
         it('parses a number from a string', function() {
             assert.strictEqual(
                 new NumberFormatter('en-us').parse('123,456.789'),
-                123456.789
+                123456.789,
             );
         });
 
         it('works with locales', function() {
             assert.strictEqual(
                 new NumberFormatter('de-de').parse('123.456,789'),
-                123456.789
+                123456.789,
             );
         });
 
         it('works with locale numerals', function() {
             assert.strictEqual(
                 new NumberFormatter('ar-eg').parse('١٢٣٬٤٥٦٫٧٨٩'),
-                123456.789
+                123456.789,
             );
         });
 
@@ -246,10 +245,10 @@ describe('NumberFormatter', function() {
                     'en-GB',
                     {
                         style: 'currency',
-                        currency: 'GBP'
-                    }
+                        currency: 'GBP',
+                    },
                 ).parse('£123,456.78'),
-                123456.78
+                123456.78,
             );
         });
 
@@ -259,24 +258,23 @@ describe('NumberFormatter', function() {
                     'de-de',
                     {
                         style: 'currency',
-                        currency: 'AUD'
-                    }
+                        currency: 'AUD',
+                    },
                 ).parse('123.456,78 AU$'),
-                123456.78
+                123456.78,
             );
         });
 
         it('throws error with invalid string', function() {
-            assert.throws(_ => {
+            assert.throws((_) => {
                 new NumberFormatter(
                     'de-de',
                     {
                         style: 'currency',
-                        currency: 'AUD'
-                    }
+                        currency: 'AUD',
+                    },
                 ).parse('INVALID');
             });
         });
     });
-
 });
